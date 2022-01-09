dc814f83ff051fc7adc78dc8687cfe9cf8cfe818
viiiyott <henry11n@naver.com>
2022-01-10 04:07:56 +0900

    Soldier wolololized

--- src/Meteor_V2/Soldier.java
+++ src/Meteor_V2/Soldier.java
@@ -10,7 +10,7 @@ public strictfp class Soldier extends Droid {
         Raid,       // Move to and attack specific enemy archon.
     }
 
-    private Mode mode = Mode.Scout;
+    private Mode mode = Mode.Wander;
     private RobotInfo[] nearbyRobots;
     private MapLocation attackTarget = null;
 
@@ -67,15 +67,29 @@ public strictfp class Soldier extends Droid {
                 break;
         }
 
+        MapLocation closest = getClosestEnemySoldier(nearbyRobots);
+        if (closest != null && !closest.equals(attackTarget) && currentLocation.distanceSquaredTo(closest) < 13) {
+            if (rc.canAttack(closest)) rc.attack(closest);
+            nextDirection = closest.directionTo(currentLocation);
+            super.move();
+        }
+
+        if (attackTarget != null && rc.isActionReady() && currentLocation.distanceSquaredTo(attackTarget) > 13) {
+            if(count(nearbyRobots) >= 0) nextDirection = currentLocation.directionTo(attackTarget);
+            else nextDirection = attackTarget.directionTo(currentLocation);
+            super.move();
+            if (rc.canAttack(attackTarget)) rc.attack(attackTarget);
+        }
+
         if (attackTarget != null && rc.canAttack(attackTarget)) {
-            rc.attack(attackTarget);
-            if (mode != Mode.Scout) {
+            if (mode != Mode.Raid || !attackTarget.equals(target)) {
                 nextDirection = attackTarget.directionTo(currentLocation);
                 if (rc.canSenseLocation(currentLocation.add(nextDirection))) {
                     int r = rc.senseRubble(currentLocation.add(nextDirection)) / 10;
                     if (r >= rc.senseRubble(currentLocation) / 10 + 2) nextDirection = Direction.CENTER;
                 }
             }
+            rc.attack(attackTarget);
         }
 
         super.move();
@@ -97,10 +111,10 @@ public strictfp class Soldier extends Droid {
             if (robot.getTeam() == rc.getTeam()) continue;
 
             int distance = currentLocation.distanceSquaredTo(robot.location);
-            if(mode != Mode.Wander && distance > 13) continue;
+            //if(mode != Mode.Wander && distance > 13) continue;
 
             int health = robot.getHealth();
-            if(robot.getType() == RobotType.ARCHON) { health -= 100000; }
+            if(robot.getType() == RobotType.ARCHON) { health -= 500; }
 
             if(health < minHealth) {
                 minHealth = health;
@@ -109,12 +123,47 @@ public strictfp class Soldier extends Droid {
         }
     }
 
+    private int count(RobotInfo[] nearbyRobots) {
+        int cnt = 0;
+        for(RobotInfo robot : nearbyRobots) {
+            if (robot.getType() != RobotType.SOLDIER) continue;
+            if (robot.getTeam() == rc.getTeam()) cnt++;
+            else cnt--;
+        }
+        return cnt;
+    }
+
+    private MapLocation getClosestEnemySoldier(RobotInfo[] nearbyRobots) {
+        int minDistance = INF;
+        MapLocation closest = null;
+
+        for(RobotInfo robot : nearbyRobots) {
+            if (robot.getTeam() == rc.getTeam()) continue;
+            if (robot.getType() != RobotType.SOLDIER) continue;
+
+            int distance = currentLocation.distanceSquaredTo(robot.location);
+
+            if(distance < minDistance) {
+                minDistance = distance;
+                closest = robot.location;
+            }
+        }
+
+        return closest;
+    }
+
     protected void selectRandomTarget() throws GameActionException {
+        int x = RNG.nextInt(rc.getMapWidth()) / 5 * 5;
+        int y = RNG.nextInt(rc.getMapHeight()) / 5 * 5;
+        target = new MapLocation(x, y);
+    }
+
+    /*protected void selectRandomTarget() throws GameActionException {
         int t = Math.max(rc.getRoundNum() / 5, 5);
         int x1 = Math.max(parentArchonLocation.x - t, 0);
         int y1 = Math.max(parentArchonLocation.y - t, 0);
         int x2 = Math.min(parentArchonLocation.x + t, rc.getMapWidth() - 1);
         int y2 = Math.min(parentArchonLocation.y + t, rc.getMapHeight() - 1);
         target = new MapLocation(RNG.nextInt(x2 - x1 + 1) + x1, RNG.nextInt(y2 - y1 + 1) + y1);
-    }
+    }*/
 }
